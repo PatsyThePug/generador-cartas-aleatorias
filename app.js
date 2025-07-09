e0118d2a55e70a55cf8fd1770753972383d1b429
Patsy The Pug_dev <mettabhavanatrance@gmail.com>
2025-07-08 23:19:02 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,17 +1,15 @@
 window.onload = function () {
-    const suits = ["♠", "♥", "♣", "♦"];
-    const values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
-  
-    const randomSuit = suits[Math.floor(Math.random() * suits.length)];
-    const randomValue = values[Math.floor(Math.random() * values.length)];
-  
-    const card = document.getElementById("card");
-  
-    card.querySelector(".top-suit").textContent = randomSuit;
-    card.querySelector(".number").textContent = randomValue;
-    card.querySelector(".bottom-suit").textContent = randomSuit;
-  
-    const isRed = randomSuit === "♥" || randomSuit === "♦";
-    card.style.color = isRed ? "red" : "black";
-  };
-  
+  const suits = ["♦", "♥", "♠", "♣"];
+  const numbers = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+
+  const randomSuit = suits[Math.floor(Math.random() * suits.length)];
+  const randomNumber = numbers[Math.floor(Math.random() * numbers.length)];
+
+  const isRed = randomSuit === "♦" || randomSuit === "♥";
+
+  document.querySelector(".top-suit").innerHTML = randomSuit;
+  document.querySelector(".number").innerHTML = randomNumber;
+  document.querySelector(".bottom-suit").innerHTML = randomSuit;
+
+  document.querySelector(".card").style.color = isRed ? "red" : "black";
+};
